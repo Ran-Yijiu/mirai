6547d85da2530e5669e36b27776280122d9d0234
Ran-Yijiu <125660377+Ran-Yijiu@users.noreply.github.com>
2024-02-16 19:56:20 +0800

    Update PluginMain.kt

--- src/main/kotlin/PluginMain.kt
+++ src/main/kotlin/PluginMain.kt
@@ -35,14 +35,14 @@ import net.mamoe.mirai.utils.info
 object PluginMain : KotlinPlugin(
     JvmPluginDescription(
         id = "org.example.mirai-example",
-        name = "插件示例",
+        name = "签到",
         version = "0.1.0"
     ) {
-        author("作者名称或联系方式")
+        author("染一久")
         info(
             """
-            这是一个测试插件, 
-            在这里描述插件的功能和用法等.
+            这是一个登录插件, 
+            目前测试中
         """.trimIndent()
         )
         // author 和 info 可以删除.
